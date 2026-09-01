20c4d69a78810f53e02afcb81c71d56901582e2e
Rafaeldo15 <rafaelosilva61@gmail.com>
2026-09-01 19:58:06 -0300

    conteudo de operacoes aritmeticos

---
+++ Aulas/Media aritmetica.py
@@ -0,0 +1,23 @@
+# operadores matemáticos
+
+numero1 = 10
+numero2 = 20
+
+#soma
+soma = numero1 + numero2
+
+#subtração
+subtracao = numero1 - numero2
+
+#multiplicação
+multiplicacao = numero1 * numero2
+
+#divisao
+divisao = numero1 / numero2
+resto = numero1 % numero2
+
+print("soma dos numeros: ", soma)
+print("subtraco dos numeros: ", subtracao)
+print("multiplicacao dos numeros: ", multiplicacao)
+print("divisao dos numeros: ", divisao)
+print("resto dos numeros: ", resto)
