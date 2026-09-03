e3a7a9fe2f2a249bc58f91ce1d37aacdfeb1327f
Rafaeldo15 <rafaelosilva61@gmail.com>
2026-09-02 21:47:09 -0300

    upando questões do dia 02/09

---
+++ Aulas/operadores_logicos.py
@@ -0,0 +1,50 @@
+
+"""
+ATRIBUICAO
+= - variavel = 10
+! - NÃO, NOT, CONTRÁRIO
+ SIM -> !SIM = NÃO
+
+#TRUE OR FALSE
+COMPARACAO
+!= SE FOR DIFERENTE RETORNA TRUE, SE FOR IGUAL RETORNA FALSE
+== SE FOR IGUAL RETORNA TRUE, SE FOR DIFERENTE FALSE
+> se for maior retorna true, se for menor retorna false
+< se for menor retorna true, se for maior retorna false
+>= se for maior ou igual retorna TRUE, se for menor ou igua retorna FALSE
+<= se for menor ou igual retorna TRUE, se for maior ou igua retorna FALSE
+"""
+
+#teste 1
+
+"""
+idade=18
+
+print (idade == 18) # true
+print(idade != 18) #false
+print(idade > 18) #false
+print(idade < 18) #false
+print (idade >= 18) #true
+print(idade <= 18) # true
+"""
+
+#para mais comparações use ( and )
+# and -> se todas comparações forem TRUE, retorna TRUE
+# or -> se ao menos 1 das comparações for true,
+
+
+# exemplo
+
+idade = 18 #inteiro
+pais_acompanham = False
+print ("nossa balada aceita adulto, idoso, não aceita crianca nem adolescente.")
+print ("voce pode entrar em uma balada?")
+#adulto = 18
+#idoso = 65
+#criança = entre 0 e 10
+#adolescente = entre 11 e 17
+
+print (idade >= 18 and idade < 70)
+
+
+
