8d23da04a1dbd705843bb8fdeb566ab880dcaa60
Rafaeldo15 <rafaelosilva61@gmail.com>
2026-09-04 20:31:48 -0300

    finalizando atividade_3

---
+++ Atividades/atividade_4/questao_1.py
@@ -0,0 +1,8 @@
+num1= int(input("digite um numero inteiro: "))
+res= num1%2
+if res == 0:
+    print("O número inserido é par")
+else:
+    print("O número inserido é ímpar")
+
+#qualquer numero par dividio por 2 igual a 0
--- Atividades/atividade_4/questao_5.py
+++ Atividades/atividade_4/questao_5.py
@@ -1,3 +1,9 @@
 idade= int(input("Digite sua idade: "))
 vip= int(input("você é VIP? Digite 1 para sim 0 para nao: "))
-org= in(input("voce é organizador? Digite 1 para sim 0 para nao: "))
+org= int(input("voce é organizador? Digite 1 para sim 0 para nao: "))
+
+if idade >= 18 or vip == 1 or org == 1:
+    print("Você pode entrar")
+
+else:
+    print("você não pode entrar")
