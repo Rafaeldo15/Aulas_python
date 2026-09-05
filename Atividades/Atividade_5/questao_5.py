2c91f9997850f657749b7ecc7ec4feb992abf0e6
Rafaeldo15 <rafaelosilva61@gmail.com>
2026-09-04 21:47:37 -0300

    atividades 5. inserção

--- Atividades/Atividade_5/questao_5.py
+++ Atividades/Atividade_5/questao_5.py
@@ -2,15 +2,19 @@ num_1 = int(input("digite um numero: "))
 opera = input("digite uma operação: (+,-,*,/): ")
 num_2 = int(input("digite outro numero: "))
 
- match opera:
+match opera:
      case "+":
-         print(f"{num_1} + {num_2} = {num_1 + num_2}")
+         print(f" O resultado da operação é= {num_1 + num_2}")
 
      case "-":
-         print(f"{num_1} - {num_2} = {num_1 - num_2}")
+         print(f"O resultado da operação é== {num_1 - num_2}")
 
-     Case "*":
-         print(f"{num_1} * {num_2} = {num_1 * num_2}")
+     case "*":
+         print(f"O resultado da operação é= {num_1 * num_2}")
 
-     Case "/":
-         print(f"{num_1} / {num_2} = {num_1 / num_2}")
+     case "/":
+         print(f"O resultado da operação é= {num_1 / num_2}")
+
+     case _:
+         print("Digite uma operação válida!")
+
